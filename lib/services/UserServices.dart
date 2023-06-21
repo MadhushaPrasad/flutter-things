43e01a8a578daaed50003f65034749bce320cf9b
MadhushaPrasad <madushaprasad@gmail.com>
2023-06-21 14:26:00 +0530

    service provider form done

---
+++ lib/services/UserServices.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+class UserServices {
+  var client = http.Client();
+  Uri baseURL = Uri.parse("https://api.dev.lifttaxi.lk/users/");
+
+  Future<http.Response> postUserDetails(String url, body, token) async {
+    var service_providerURL = baseURL.toString() + url;
+    var _headers = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization': "Bearer ${token}"
+    };
+
+    String rawJson = jsonEncode(body);
+    return await client.post(Uri.parse(service_providerURL),
+        headers: _headers, body: rawJson);
+  }
+}
